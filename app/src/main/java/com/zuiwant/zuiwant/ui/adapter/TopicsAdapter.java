0a5909e3ebb1cabd8b49aeedfb01be9809a5410c
bingtaoli <1464738780@qq.com>
2016-04-14 15:31:30 +0800

    topic item增加click事件; 重构部分代码

--- app/src/main/java/com/zuiwant/zuiwant/ui/adapter/TopicsAdapter.java
+++ app/src/main/java/com/zuiwant/zuiwant/ui/adapter/TopicsAdapter.java
@@ -2,103 +2,56 @@ package com.zuiwant.zuiwant.ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import com.zuiwant.zuiwant.R;
 import com.zuiwant.zuiwant.model.TopicModel;
-import com.zuiwant.zuiwant.utils.OnScrollToBottomListener;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by libingtao on 16/4/13.
  */
-public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.ViewHolder> {
+public class TopicsAdapter extends BaseRecycleAdapter {
 
-    Context mContext;
-    OnScrollToBottomListener mListener;
-    ArrayList<TopicModel> mTopics = new ArrayList<TopicModel>();
+    private List<TopicModel> topics;
 
-    public TopicsAdapter (Context context, ArrayList<TopicModel> topics){
-        mContext = context;
-        mTopics = topics;
+    public TopicsAdapter(Context context, List<TopicModel> topics) {
+        super(context);
+        this.topics = topics;
     }
 
-    public TopicsAdapter(Context context, OnScrollToBottomListener listener) {
-        mContext = context;
-        mListener = listener;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_topic, viewGroup, false);
-        return new ViewHolder(v);
+        return new TopicViewHolder(v);
     }
 
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int i) {
-        final TopicModel topic = mTopics.get(i);
-
+    public void onBindViewHolder(TopicViewHolder viewHolder, int i) {
+        final TopicModel topic = topics.get(i);
         viewHolder.topicName.setText(topic.topicName);
         viewHolder.articleNum.setText(String.valueOf(topic.articleNum));
         viewHolder.topicIntro.setText(topic.topicIntro);
-
     }
 
     @Override
     public int getItemCount() {
-        return mTopics.size();
+        return topics.size();
     }
 
-    public void insertAtBack(ArrayList<TopicModel> data, boolean merge) {
-        if (merge)
-            mTopics.addAll(data);
-        else
-            mTopics = data;
-        notifyDataSetChanged();
-    }
+    public class TopicViewHolder extends BaseViewHolder {
 
-    public void update(ArrayList<TopicModel> data, boolean merge) {
-        if (merge && mTopics.size() > 0) {
-            for (int i = 0; i < mTopics.size(); i++) {
-                TopicModel obj = mTopics.get(i);
-                boolean exist = false;
-                for (int j = 0; j < data.size(); j++) {
-                    if (data.get(j).topicId == obj.topicId) {
-                        exist = true;
-                        break;
-                    }
-                }
-                if (exist) continue;
-                data.add(obj);
-            }
-        }
-        mTopics = data;
-
-        notifyDataSetChanged();
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder {
-
-        /**
-         * 每个item view中只有3个显示
-         * 背景图后续补上
-         */
         public TextView topicName;
         public TextView articleNum;
         public TextView topicIntro;
 
-        public ViewHolder(View view) {
-            super(view);
-            topicName = (TextView) view.findViewById(R.id.topic_name);
-            topicIntro = (TextView) view.findViewById(R.id.topicIntro);
-            articleNum = (TextView) view.findViewById(R.id.txt_articles);
+        public TopicViewHolder(View itemView) {
+            super(itemView);
+            topicName = (TextView) itemView.findViewById(R.id.topic_name);
+            topicIntro = (TextView) itemView.findViewById(R.id.topicIntro);
+            articleNum = (TextView) itemView.findViewById(R.id.txt_articles);
         }
-
     }
 
 }
--- app/src/main/java/com/zuiwant/zuiwant/ui/fragment/TopicFragment.java
+++ app/src/main/java/com/zuiwant/zuiwant/ui/fragment/TopicFragment.java
@@ -1,21 +1,20 @@
 package com.zuiwant.zuiwant.ui.fragment;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.TypedValue;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 import com.zuiwant.zuiwant.R;
 
 import com.zuiwant.zuiwant.model.TopicModel;
+import com.zuiwant.zuiwant.ui.adapter.BaseRecycleAdapter;
 import com.zuiwant.zuiwant.ui.adapter.TopicsAdapter;
 
 import java.util.ArrayList;
@@ -43,14 +42,26 @@ public class TopicFragment extends BaseFragment {
         testList.add(new TopicModel(true));
         testList.add(new TopicModel(true));
 
-        mTopicAdapter = new TopicsAdapter(context, testList);
+        //TODO 这两步可以封在一个initView函数中,类似ListenerRain项目做的一样
+        mSwipeLayout = (SwipeRefreshLayout) layout.findViewById(R.id.swipe_container);
         mRecyclerView = (RecyclerView) layout.findViewById(R.id.list_topics);
 
         mLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+
+        //TODO 可以封在setViewStatus函数中
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mTopicAdapter = new TopicsAdapter(context, testList);
         mRecyclerView.setAdapter(mTopicAdapter);
 
-        mSwipeLayout = (SwipeRefreshLayout) layout.findViewById(R.id.swipe_container);
+        mTopicAdapter.setOnItemClickListener(new BaseRecycleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                //打开一个新的activity
+                //暂时只记录log
+                Log.d("lee", "one item is clicked");
+            }
+        });
+
         mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -60,6 +71,4 @@ public class TopicFragment extends BaseFragment {
 
         return layout;
     }
-
-
 }
